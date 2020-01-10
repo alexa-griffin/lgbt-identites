45efb7114ddffbdab2f81262c5494f40d697d7fb
alexa <alexa.griffin42@gmail.com>
2020-01-10 16:33:07 -0500

    fix some shit

--- notes.md
+++ notes.md
@@ -3,4 +3,6 @@
 - expand the `Html` component
 - get rid of the leading line in every precreated file
 - source props when passed as arguements do not get processed properly
-  - make search regex case insensative
+  - ~~make search regex case insensative~~
+  - ~~this is especially important for links~~
+- ~~source props do not handle urls right~~
--- site/index.jsx
+++ site/index.jsx
@@ -2,5 +2,12 @@ import Html from "components/Html.jsx"
 
 export default props => 
 <Html stylesheet-href="assets/style/home.sass" title="Lgbt Identities">
-  <h1></h1>
+  <header>
+    
+  </header>
+  <section className="hero">
+    <h1>LGBTQ+ Identites and Orientations</h1>
+    <p>Because people have trouble remembering every identity or orientation out there I have created this website as a reference.</p>
+    <p>If there is something I have missed or am mistaken please feel free to <a href="https://github.com/alexa-griffin/lgbt-identites/issues">open an issue</a></p>
+  </section>
 </Html>
